1b3d8b6580f212993c27e3f3bd8de3a78ce1524c
David Lewis and Michael Albers <michael@>
2018-01-24 11:50:33 -0800

    add js make players, generate dice, push dice total to player objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,53 @@
+// Business
+function Player(name, roll, total) {
+  this.name = name
+  this.diceRolls = roll
+  this.total = total
+}
+
+function diceRoll(Player) {
+  var dice = Math.floor(Math.random() * (6)+1);
+  if (dice === 1) {
+    Player.diceRolls = [];
+  } else {
+    Player.diceRolls.push(dice);
+    return Player;
+  }
+}
+
+function hold (Player){
+  Player.total += Player.diceRolls.reduce(function(a,b){
+    return a + b;
+  });
+}
+
+// User
 $(document).ready(function() {
-  
+var players = [];
+
+
+
+$("#newPlayer").submit(function (event) {
+  event.preventDefault();
+  var newPlayer = new Player("", [], 0);
+  newPlayer.name = $("#playerName").val();
+  players.push(newPlayer)
+  console.log(players);
+
+});
+
+
+
+$("#roll").click(function() {
+  diceRoll(players[0]);
+  console.log(players[0]);
+});
+
+$("#hold").click(function() {
+  hold(players[0]);
+  console.log(players[0]);
+});
+
+
+
 });
